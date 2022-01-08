d28e866f6a4603a4407a7084d95f8bbb00586cae
Unyq <azaharaunyq@MacBook-Air-de-Unyq.local>
2022-01-08 21:41:29 +0100

    Planteamiento

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const buttonValor = document.querySelector(".js-valor");
+const buttonSubmit = document.querySelector(".js-submit");
+const buttonBorrar = document.querySelector(".js-borrar");
+const buttonCuenta = document.querySelector(".js-cuenta");
+const buttonDia = document.querySelector(".js-dia");
+
