892714acef668c39e681aa340bea483bb766279a
Unyq <azaharaunyq@MacBook-Air-de-Unyq.local>
2022-01-19 20:56:41 +0100

    Azahara

---
+++ js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+//const submit = document.querySelector(".js-submit");
+//const borrar = document.querySelector(".js-borrar");
+//let dia = document.querySelector(".js-dia");
+
+/*
+function buttonValor() {
+    let valor = document.querySelector("#barra").value;
+alert (`Tu valoración es de ${valor} puntos sobre 4`);
+}
+const accountBotton = document.querySelector (".bt-cuenta");
+accountBotton.addEventListener("click", buttonValor);
+
+function buttonCuenta() {
+    let pais = document.querySelector("#pais").value
+    let controlIBAN = document.querySelector("#controlIBAN").value
+    let entidad = document.querySelector("#entidad").value
+    let sucursal = document.querySelector("#sucursal").value
+    let dc = document.querySelector("#dc").value
+    let cuenta = document.querySelector("#cuenta").value
+    alert(
+        `Le informamos de que su número de cuenta es: ${pais}-${controlIBAN}-${entidad}-${sucursal}-${dc}-${cuenta}`
+      );
+}
+*/
+function verValor() {
+    let valor = document.querySelector("#barra").value;
+alert (`Tu valoración es de ${valor} puntos sobre 4`);
+}
--- main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const buttonValor = document.querySelector(".js-valor");
-const buttonSubmit = document.querySelector(".js-submit");
-const buttonBorrar = document.querySelector(".js-borrar");
-const buttonCuenta = document.querySelector(".js-cuenta");
-const buttonDia = document.querySelector(".js-dia");
-
