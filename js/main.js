36d472ad00959e347d34a0c3809306c13ee8b1f8
AzaharaGV <98090482+AzaharaGV@users.noreply.github.com>
2022-01-29 19:05:44 +0100

    First Step Javascrip

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
-"use strict";
-
-
-//const submit = document.querySelector(".js-submit");
-//const borrar = document.querySelector(".js-borrar");
-//let dia = document.querySelector(".js-dia");
-
-/*
-function buttonValor() {
-    let valor = document.querySelector("#barra").value;
-alert (`Tu valoración es de ${valor} puntos sobre 4`);
-}
-const accountBotton = document.querySelector (".bt-cuenta");
-accountBotton.addEventListener("click", buttonValor);
-
-function buttonCuenta() {
-    let pais = document.querySelector("#pais").value
-    let controlIBAN = document.querySelector("#controlIBAN").value
-    let entidad = document.querySelector("#entidad").value
-    let sucursal = document.querySelector("#sucursal").value
-    let dc = document.querySelector("#dc").value
-    let cuenta = document.querySelector("#cuenta").value
-    alert(
-        `Le informamos de que su número de cuenta es: ${pais}-${controlIBAN}-${entidad}-${sucursal}-${dc}-${cuenta}`
-      );
-}
-*/
 function verValor() {
-    let valor = document.querySelector("#barra").value;
-alert (`Tu valoración es de ${valor} puntos sobre 4`);
+    let valor = document.querySelector("#valoracion").value;
+    alert(`Has valorado con ${valor} puntos`);
+  }
+
+  function verCuenta() {
+    let pais = document.querySelector("#pais").value;
+    let controlIBAN = document.querySelector("#controlIBAN").value;
+    let entidad = document.querySelector("#entidad").value;
+    let sucursal = document.querySelector("#sucursal").value;
+    let dc = document.querySelector("#dc").value;
+    let cuenta = document.querySelector("#cuenta").value;
+    alert(
+      `Su número de cuenta es: ${pais}-${controlIBAN}-${entidad}-${sucursal}-${dc}-${cuenta}`
+    );
+  }
+
+  function verDia() {
+    let fecha_dom = document.getElementById("date").value;
+    let fecha_date = new Date(fecha_dom);
+  
+    var fecha_formato = new Intl.DateTimeFormat("es-ES", {
+      weekday: "long",
+    }).format(fecha_date);
+  
+    alert(
+      `La fecha seleccionada es ${fecha_dom}, ${fecha_formato}`
+    );
+  }
+
+  function validarCodigoPostal()
+{
+  var input = document.getElementById("cp").value;
+  console.log(parseInt(input))
+  if(input.length == 5 && parseInt(input) >= 10000 && parseInt(input) <= 52999)
+  {
+    alert("codigo valido");
+  }
+  else{
+    alert("codigo invalido");
+   }
 }
